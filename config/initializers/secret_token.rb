32a16abc132eb00c9825fa1080c7650bfacefb35
Michael Welburn <mwelbu2@gmail.com>
2012-11-17 09:04:46 -0600

    Initial project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trinkets::Application.config.secret_token = 'd54eef72a75f33bf88fb8125e3ef3dacbf4c0156bed7ab2974322b39f0afc810d9cac4d21402b4d5967f9f13830994e4d3b4c3fa5e2ee13d558d6b4ddfd227b8'
